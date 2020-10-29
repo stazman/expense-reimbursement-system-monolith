753abe25b58e7a1e58f1ee334aaebc94d3efa09d
Christopher DiStasio <cadistasio@gmail.com>
2020-10-29 09:13:59 -0500

    Adds UserDAO files

--- src/main/java/com/ers/dao/UserDAO.java
+++ src/main/java/com/ers/dao/UserDAO.java
@@ -1,12 +1,16 @@
 package com.ers.dao;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.HashSet;
+import java.util.Set;
 
-import com.ers.models.UserRole;
-import com.ers.models.User;
+import com.ers.models.*;
 import com.ers.utilities.ConnectionUtil;
+
+import javax.xml.transform.Result;
 
 public class UserDAO implements DAOControl<User, Integer>, UserDAOInterface {
 
@@ -71,10 +75,7 @@ public class UserDAO implements DAOControl<User, Integer>, UserDAOInterface {
 			
 			ResultSet rs = pstmt.executeQuery();
 
-
-
 			if (rs.next()) {
-
 
 				u.setUserId(rs.getInt("ers_usrs_id"));
 				u.setUsername(rs.getString("ers_usrnm"));
@@ -97,6 +98,72 @@ public class UserDAO implements DAOControl<User, Integer>, UserDAOInterface {
 		}
 		
 		return u;
+	}
+
+	public Set<Reimbursement> findReimbursementsByUserId (int userId){
+
+		Set<Reimbursement> allReimbs = new HashSet<>();
+
+		try (Connection conn = cu.getConnection()) {
+
+			String sql = "select * from ers_reimb where reimb_author = ?";
+
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+
+			pstmt.setInt(1, userId);
+
+			ResultSet rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+
+				Reimbursement reimb = new Reimbursement();
+				reimb.setReimbursementId(rs.getInt("reimb_id"));
+				reimb.setAmount(rs.getDouble("reimb_amt"));
+				reimb.setDescription(rs.getString("reimb_description"));
+
+				ReimbursementStatus reimbStatus = new ReimbursementStatus();
+				reimbStatus.setReimbStatusId(rs.getInt("reimb_stat_id"));
+//				reimbStatus.setReimbStatus(rs.getString("reimb_stat"));
+				reimb.setReimbStatus(reimbStatus);
+
+				ReimbursementType reimbType = new ReimbursementType();
+				reimbType.setReimbTypeId(rs.getInt("reimb_type_id"));
+//				reimbType.setReimbType(rs.getString("reimb_type"));
+				reimb.setReimbType(reimbType);
+
+				allReimbs.add(reimb);
+
+
+				return allReimbs;
+
+			}
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	public UserRole getRoleById(int id) {
+		String sql = "select * from ers_usr_roles where ers_usr_role_id = ?";
+		UserRole r = new UserRole();
+
+		try(Connection conn = cu.getConnection()){
+
+			PreparedStatement pst = conn.prepareStatement(sql);
+			pst.setInt(1, id);
+
+			ResultSet rs = pst.executeQuery();
+			if(rs.next()) {
+				r.setUserRoleId(id);
+				r.setUserRole(rs.getString(2));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return r;
 	}
 
 	@Override
@@ -144,4 +211,26 @@ public class UserDAO implements DAOControl<User, Integer>, UserDAOInterface {
 		return null;
 	}
 
+	public User checkUsernamePassword(String username, String password) {
+		String sql = "{call validateUser(?,?)}";
+
+		User u = null;
+
+		try (Connection conn = cu.getConnection()) {
+			CallableStatement cs = conn.prepareCall(sql);
+			cs.setString(1, password);
+			cs.setString(2, username);
+
+			ResultSet rs = cs.executeQuery();
+
+			if (rs.next()) {
+				UserRole ur = getRoleById(rs.getInt(7));
+				u = new User(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), ur);
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return u;
+	}
 }
--- src/main/java/com/ers/dao/UserDAOInterface.java
+++ src/main/java/com/ers/dao/UserDAOInterface.java
@@ -1,11 +1,16 @@
 package com.ers.dao;
 
+import com.ers.models.Reimbursement;
 import com.ers.models.User;
+
+import java.util.Set;
 
 public interface UserDAOInterface {
 
     User getUserByUsername(String username);
 
     User findById(Integer i);
+
+    Set<Reimbursement> findReimbursementsByUserId (int userId);
 
 }
