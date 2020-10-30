414c6659d43ee7b9709cc8a893d6292387387702
Christopher DiStasio <cadistasio@gmail.com>
2020-10-30 06:07:50 -0500

    Adds findReimbursementsByUserId()

--- src/main/java/com/ers/service/UserService.java
+++ src/main/java/com/ers/service/UserService.java
@@ -1,22 +1,19 @@
 package com.ers.service;
 
+import com.ers.models.Reimbursement;
 import com.ers.models.User;
-import com.ers.dao.DAOControl;
+
+import java.util.Set;
+
 import com.ers.dao.UserDAOInterface;
-import com.ers.dao.UserDAO;
 
 public class UserService {
 
-	private DAOControl<User, Integer> uDao;
 	private UserDAOInterface udi;
 
-	public UserService(DAOControl<User, Integer> uDao) {
+	public UserService(UserDAOInterface udi) {
 		super();
-		this.uDao = uDao;
-	}
-
-	public UserService() {
-		this(new UserDAO());
+		this.udi = udi;
 	}
 	
 	public User validateUser(String username, String password) {
@@ -26,5 +23,9 @@ public class UserService {
 	public User findById(Integer id) {
 		return udi.findById(id);
 	}
-
+	
+	public Set<Reimbursement> findReimbursementsByUserId(int userId)  {
+		return udi.findReimbursementsByUserId(userId);
+	}
+	
 }
