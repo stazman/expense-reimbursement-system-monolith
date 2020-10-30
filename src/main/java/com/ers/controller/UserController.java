e642342fc5c24507e702f5a0114fb675a3f3f856
Christopher DiStasio <cadistasio@gmail.com>
2020-10-30 05:47:43 -0500

    Adds login-related, logout methods

--- src/main/java/com/ers/controller/UserController.java
+++ src/main/java/com/ers/controller/UserController.java
@@ -2,11 +2,17 @@ package com.ers.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
+
+import com.ers.dao.UserDAOInterface;
 import com.ers.models.User;
 import com.ers.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class UserController {
 
+	ObjectMapper om = new ObjectMapper();
+	User u = new User();
+	
 	private UserService uServ;
 
 	public UserController(UserService uServ) {
@@ -18,17 +24,31 @@ public class UserController {
 		this(new UserService());
 	}
 
+	public String redirectUser(HttpServletRequest req) {
+		User u = (User) req.getSession().getAttribute("user");
+		if (u.getRole().getUserRole().equals("employee")) {
+			return "html/empl.html";
+		} else {
+			return "html/landing.html";
+		}
+	}
+	
 	public String login(HttpServletRequest req) {
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		User u = uServ.validateUser(username, password);
+			if(u == null ) {
+				return "landing.page";
+			} else {
+				req.getSession().setAttribute("user", u);
+				return redirectUser(req);
+			}
+	}
+
+	public String logout(HttpServletRequest req) {
 		return null;
 	}
 
-//	public String login(HttpServletRequest req) {
-//		String userName = req.getParameter("name");
-//		User u = uServ.findByUsername(User u);
-//		if(u != null) {
-//			return "html/monster.html";
-//		} else {
-//			return "html/landing.html";
-//		}
-//	}
 }
+
+
